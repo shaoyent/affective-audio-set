c3b397b9759f9daa3172b5f9faef926dca2af61b
Shaoyen Tseng <shaoyen.t@gmail.com>
2020-05-25 15:44:13 -0700

    Add proxy support

--- src/grab_audio.py
+++ src/grab_audio.py
@@ -1,5 +1,6 @@
 import os
 
+import glob
 import logging
 import argparse
 
@@ -11,9 +12,19 @@ import youtube_dl
 from multiprocessing import Pool
 
 def grab_audio( vid_id, save_path='./', ydl_opts={}) :
-    
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['https://www.youtube.com/watch?v={}'.format(vid_id)])
+
+    if glob.glob( os.path.join(save_path, "{}*.wav".format(vid_id))) and ydl_opts.get('nooverwrites', True) :
+        logging.warning("Audio with ID {} exists in path. Enable overwrite to re-download".format(vid_id))
+
+    else :
+        try :
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download(['https://www.youtube.com/watch?v={}'.format(vid_id)])
+        except Exception as e :
+            logging.error(e)
+            return False
+
+    return True
 
 
 def main():
@@ -22,11 +33,13 @@ def main():
 
     parser.add_argument("--csv", type=str, default=None, required=True, help='Comma-separated file of video IDs')
     parser.add_argument("--save_path", type=str, default='./downloaded_files', help='Path to save downloaded files')
-    parser.add_argument("--nj", type=int, default=4, help='Number of parallerl jobs')
+    parser.add_argument("--proxy", type=str, default=None, help='Proxy to use for youtube-dl')
+    parser.add_argument("--nj", type=int, default=4, help='Number of parallel jobs')
+    parser.add_argument("--overwrite", default=False, action="store_true", help='Enable overwriting files')
 
     args = parser.parse_args()
 
-    logging.basicConfig(filename='grab_audio.log', filemode='w', level=logging.INFO)
+    logging.basicConfig(filename='grab_audio.log', filemode='a', level=logging.DEBUG)
     logger = logging.getLogger()
     logger.info(args)
 
@@ -52,20 +65,22 @@ def main():
         'writeinfojson': True,
         'writedescription': True,
         'writesubtitles': True,
+        'nooverwrites': not args.overwrite,
     }
+    logger.debug(ydl_opts)
+
+    if args.proxy is not None :
+        ydl_opts.update( {'proxy': args.proxy} )
 
     pool_fn = partial(grab_audio,
                     save_path=args.save_path,
                     ydl_opts=ydl_opts
                 )
 
+    logger.info("Grabbing audio from {}".format(args.csv))
     with Pool(args.nj) as pool :
 
-        ret = tqdm( pool.imap_unordered(pool_fn, vid_ids), total=len(vid_ids)) 
-
-        for r in ret :
-            continue
-
+        ret = list(tqdm( pool.imap_unordered(pool_fn, vid_ids), total=len(vid_ids)))
 
 if __name__ == "__main__" :
     main()
